f14dd493b8308f44c4d5b9781dc034c7945e0211
Dave Goddard <dave@goddard.id.au>
2020-06-11 21:26:02 +1000

    attempt to use mailjet for email

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/mailjet/mailjet-apiv3-go v0.0.0-20190724151621-55e56f74078c
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 )
--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	mailjet "github.com/mailjet/mailjet-apiv3-go"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 )
 
 func main() {
+	reClean := regexp.MustCompile(`[\n\r\s]*\n+[\n\r\s]*`)
+
 	resp, err := http.Get("https://grabit.clubwyndhamsp.com/page/7/Packages")
 	if err != nil {
 		log.Fatal(err)
@@ -21,10 +25,10 @@ func main() {
 		log.Fatal(err)
 	}
 
+	fullText := ""
 	doc.Find(".listing").Each(func(i int, s *goquery.Selection) {
 		body := ""
 		s.Find(".listing-header,h4,p").Each(func(_ int, s *goquery.Selection) {
-			// TODO: Ignore style="display:none;"
 			if style, exists := s.Attr("style"); exists {
 				if style == "display:none;" {
 					return
@@ -33,10 +37,41 @@ func main() {
 			body += s.Text() + "\n"
 		})
 		body = strings.TrimSpace(body)
-		body = regexp.MustCompile(`[\n\r\s]*\n+[\n\r\s]*`).ReplaceAllString(body, "\n")
+		body = reClean.ReplaceAllString(body, "\n")
 
-		// body := s.Text()
-		fmt.Println(body)
-		fmt.Println("========")
+		fullText += body + "\n============\n"
 	})
+
+	fmt.Println(fullText)
+	sendEmail(fullText)
+}
+
+func sendEmail(text string) {
+	mailjetClient := mailjet.NewMailjetClient(os.Getenv("MJ_APIKEY_PUBLIC"), os.Getenv("MJ_APIKEY_PRIVATE"))
+	messagesInfo := []mailjet.InfoMessagesV31{
+		mailjet.InfoMessagesV31{
+			From: &mailjet.RecipientV31{
+				Email: "dave@godard.id.au",
+				Name:  "Dave Goddard",
+			},
+			To: &mailjet.RecipientsV31{
+				mailjet.RecipientV31{
+					Email: "dave@godard.id.au",
+					Name:  "Dave Goddard",
+				},
+				mailjet.RecipientV31{
+					Email: "catherine@lypc.com.au",
+					Name:  "Catherine Garro",
+				},
+			},
+			Subject:  "Grabit - Worldmarksp.com",
+			TextPart: text,
+		},
+	}
+	messages := mailjet.MessagesV31{Info: messagesInfo}
+	res, err := mailjetClient.SendMailV31(&messages)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Data: %+v\n", res)
 }
